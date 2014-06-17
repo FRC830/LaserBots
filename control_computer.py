39a6306a14ab3a8cd7f5e1979be8dd342fd83e87
werehyena <scottemorton@comcast.net>
2014-06-16 21:33:06 -0400

    added files for two main programs

---
+++ control_computer.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+#This is the program that runs on the laptop and sends information from the joysticks to the raspberry pis on the cars
---
+++ lasercar.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+#This is the program that runs on each car and takes input from the control laptop
