81e64e6af0e768a3476c78e09d0a31ce6e72620d
werehyena <scottemorton@comcast.net>
2014-06-19 19:05:46 -0400

    copied over some pika stuff

--- control_computer.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-
-#This is the program that runs on the laptop and sends information from the joysticks to the raspberry pis on the cars
--- lasercar.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-
-#This is the program that runs on each car and takes input from the control laptop
