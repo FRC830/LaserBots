ab622b98f9b238f4f8babbc96ca023222eca7865
lethosor <lethosor@gmail.com>
2014-06-20 09:23:27 -0400

    Disconnect clients when server quits

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import socket
 import sys
 import threading
+import time
 
 HOST = ''
 PORT = 50007
@@ -15,17 +16,47 @@ class Server:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((host, port))
         self.socket.listen(4)
+        self.callbacks = {
+            'connect': [],
+            'disconnect': [],
+            'message': [],
+        }
 
     def serve_forever(self):
         while True:
             ClientHandler(self, *self.socket.accept()).start()
 
+    def remove_client(self, client):
+        self.lock.acquire()
+        if client in self.clients:
+            self.clients.remove(client)
+        self.lock.release()
+
+    def terminate(self):
+        for c in self.clients:
+            c.socket.shutdown(socket.SHUT_RD)
+            c.close()
+        self.socket.close()
+
+    def add_callback(self, event, func):
+        if not hasattr(func, '__call__'):
+            raise TypeError('Function must be callable')
+        if event in self.callbacks:
+            if not func in self.callbacks[event]:
+                self.callbacks[event].append(func)
+            else:
+                raise ValueError('Function %r already exists for event %r' % (func, event))
+        else:
+            raise ValueError('Unknown event: %r' % event)
+
 class ClientHandler(threading.Thread):
     def __init__(self, server, client_socket, remote_address):
         super(ClientHandler, self).__init__()
         self.server, self.socket, self.addr = server, client_socket, remote_address
+        self.open = False
 
     def run(self):
+        self.open = True
         self.server.lock.acquire()
         self.server.clients.append(self)
         self.server.lock.release()
@@ -35,28 +66,41 @@ class ClientHandler(threading.Thread):
             if not data:
                 break
             print('%s:%s: %s' % (self.addr[0], self.addr[1], data))
-        self.socket.close()
-        print '* %s:%s disconnected.' % self.addr
-        self.server.lock.acquire()
-        self.server.clients.remove(self)
-        self.server.lock.release()
+        self.close()
 
-def main():
-    try:
-        print('Starting server on %s:%s...' % (HOST, PORT))
-        server = Server(HOST, PORT)
-    except socket.error as e:
-        print('Failed to initialize server: %s' % e)
-        return False
+    def close(self):
+        if self.open:
+            try:
+                #self.socket.shutdown(socket.SHUT_RD)
+                self.socket.close()
+            except Exception as e:
+                import traceback
+                traceback.print_exc()
+                print('%s' % e)
+            self.server.remove_client(self)
+            print '* %s:%s disconnected.' % self.addr
+            self.open = False
+
+def main(server):
+    print('Starting server on %s:%s...' % (HOST, PORT))
     try:
         server.serve_forever()
     except Exception as e:
         print('%s: %s' % (type(e), e))
-        return False
+        return False, e
     except KeyboardInterrupt, SystemExit:
         print('\nExiting')
-        return True
+        server.terminate()
+        return True, None
 
 if __name__ == '__main__':
-    if not main():
+    try:
+        server = Server(HOST, PORT)
+    except socket.error as e:
+        print('Failed to initialize server: %s' % e)
         sys.exit(1)
+    try:
+        main(server)
+    except (Exception, KeyboardInterrupt, SystemExit) as e:
+        print(e)
+        server.socket.close()
