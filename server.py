deb9fe92e599507cf8f27b39ab1d879ec2beee43
werehyena <morton.scott.e@gmail.com>
2014-06-19 20:51:25 -0400

    rm old server

--- server.py
+++ server.py
@@ -9,17 +9,6 @@ PORT_NUMBER = 5000
 HOST = ''
 PORT = 50007
 
-#sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-#sock.bind( (HOST, PORT) )
-#sock.listen(1)
-#conn, addr = sock.accept()
-#print 'Connected by', addr
-
-#while True:
-#        data = conn.recv(SIZE)
-#        if not data: break
-#        print data
-#conn.close()
 import SocketServer
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
