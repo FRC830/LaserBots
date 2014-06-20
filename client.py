7f60f68416c34cb8d065eed2e65ef986b902e4c8
lethosor <lethosor@gmail.com>
2014-06-19 22:00:59 -0400

    Basic multiple-client server

--- client.py
+++ client.py
@@ -6,7 +6,12 @@ import sys
 import time
 import socket
 
-SERVER_IP   = sys.argv[1] if len(sys.argv) > 1 else raw_input('Remote IP: ')
+try:
+    import readline
+except ImportError:
+    pass
+
+SERVER_IP = sys.argv[1] if len(sys.argv) > 1 else raw_input('Remote IP: ')
 PORT_NUMBER = 50007
 SIZE = 1024
 
@@ -16,6 +21,12 @@ except socket.error as e:
     print('Failed to connect to server (%s)' % SERVER_IP)
     sys.exit()
 sock.connect((SERVER_IP,PORT_NUMBER))
-sock.sendall('Hello, world')
+while True:
+    try:
+        line = raw_input('> ')
+        sock.sendall(line)
+    except KeyboardInterrupt:
+        print('')
+        break
 
 sock.close()
--- server.py
+++ server.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python
 
-#This is the program that runs on the laptop and sends information from the joysticks to the raspberry pis on the cars
 import socket
 import sys
-PORT_NUMBER = 5000
-#SIZE = 1024
+import threading
 
 HOST = ''
 PORT = 50007
 
-import SocketServer
+class Server:
+    """ Basic socket server """
+    def __init__(self, host, port):
+        self.lock = threading.Lock()
+        self.clients = []
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((host, port))
+        self.socket.listen(4)
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
+    def serve_forever(self):
+        while True:
+            ClientHandler(self, *self.socket.accept()).start()
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+class ClientHandler(threading.Thread):
+    def __init__(self, server, client_socket, remote_address):
+        super(ClientHandler, self).__init__()
+        self.server, self.socket, self.addr = server, client_socket, remote_address
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        # self.request.sendall(self.data.upper())
+    def run(self):
+        self.server.lock.acquire()
+        self.server.clients.append(self)
+        self.server.lock.release()
+        print('* %s:%s connected' % self.addr)
+        while True:
+            data = self.socket.recv(1024)
+            if not data:
+                break
+            print('%s:%s: %s' % (self.addr[0], self.addr[1], data))
+        self.socket.close()
+        print '* %s:%s disconnected.' % self.addr
+        self.server.lock.acquire()
+        self.server.clients.remove(self)
+        self.server.lock.release()
 
-if __name__ == "__main__":
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-    server.serve_forever()
+def main():
+    try:
+        print('Starting server on %s:%s...' % (HOST, PORT))
+        server = Server(HOST, PORT)
+    except socket.error as e:
+        print('Failed to initialize server: %s' % e)
+        return False
+    try:
+        server.serve_forever()
+    except Exception as e:
+        print('%s: %s' % (type(e), e))
+        return False
+    except KeyboardInterrupt, SystemExit:
+        print('\nExiting')
+        return True
 
+if __name__ == '__main__':
+    if not main():
+        sys.exit(1)
