6cbd9e2ed0f6b7f33b650bdef60b6fc8723278b7
werehyena <morton.scott.e@gmail.com>
2014-06-22 14:32:07 -0400

    joysticks work now

--- car_controller.py
+++ car_controller.py
@@ -7,11 +7,32 @@
 #the server should call data_to_send() and send the output to the car
 #then take input from the car and give it as input to data_received()
 
+import sys
 import pygame as pg
 pg.joystick.init()
 
+LEFT_Y = 1
+LEFT_X = 0
+RIGHT_Y = 3
+RIGHT_X = 4
+#left bumper positive, right bumper negative
+BUMPER_AXIS = 2
+
+BUTTON_A = 0
+BUTTON_B = 1
+BUTTON_X = 2
+BUTTON_Y = 3
+BUTTON_LB = 4
+BUTTON_RB = 5
+BUTTON_BACK = 6
+BUTTON_START = 7
+LEFT_STICK = 8
+RIGHT_STICK = 9
+
+
 class CarController:
     def __init__(self, joy_id, client, dispatcher):
+        pg.init()
         self.joy = None
         self.client, self.dispatcher = client, dispatcher
         self.id = joy_id
@@ -19,6 +40,7 @@ class CarController:
         self.controller_list = []  # list of all controllers
         if pg.joystick.get_count() > joy_id:
             self.joy = pg.joystick.Joystick(joy_id)
+            self.joy.init()
         else:
             print('Joystick %i not detected' % (joy_id))
 
@@ -26,9 +48,9 @@ class CarController:
     def loop(self):
         pg.event.pump()
         if self.joy:
-            # arbitrary controls for now
-            speed = joy.get_axis(1)
-            turn = joy.get_axis(4)
+            #negative values are up on the y-axes
+            speed = -self.joy.get_axis(LEFT_Y)
+            turn = self.joy.get_axis(RIGHT_X)
         else:
             speed = turn = 0
         self.send({'speed': speed, 'turn': turn})
