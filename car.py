717fba5c139bbe5e8c73afef664e33c8e2a66190
werehyena <morton.scott.e@gmail.com>
2014-06-26 20:00:45 -0400

    fixed output

--- car.py
+++ car.py
@@ -54,7 +54,7 @@ class Car:
             if 'health' in data:
                 self.update_health(data['health'])
             if 'fire' in data:
-                print('fire: %b' % data['fire'], end = '')
+                print('fire: %i ' % data['fire'], end = '')
             if 'speed' in data:
                 self.drive_motor.set_speed(data['speed'])
                 print('speed: %f' % data['speed'], end='')
